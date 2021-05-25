29d59cb7e956ef35a786388aeabfa1b5a7899532
Srividya-raveendran <84705334+Srividya-raveendran@users.noreply.github.com>
2021-05-25 14:06:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Gallery from Folder Demo</title>
-<style type="text/css">
-<!--
-li{
-	list-style-type:none;
-	margin-right:10px;
-	margin-bottom:10px;
-	float:left;
-}
-
--->
-</style></head>
-
-<body>
-
 <ul>
 	<?php
+		echo "inside index.php"
 		$dirname = "img/";
 		$images = scandir($dirname);
 		shuffle($images);
@@ -31,5 +13,3 @@ li{
 	?>
 </ul>
 
-</body>
-</html>
